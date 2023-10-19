1e79f0247c06258fa9168c4b54c1279bfadf6df4
jihye <bnm1354@gmail.com>
2023-10-20 00:03:14 +0900

    Skills Section 수정

--- src/pages/Skill.js
+++ src/pages/Skill.js
@@ -1,37 +1,43 @@
-function Skill(){
+import React, {forwardRef} from 'react';
+import '../assets/scss/pages/_skill.scss'
+
+function Skill(props, ref){
+
   return(
-    <>
-    
-    <div className="group_circle">
-      <h2 className="title_h2">SKILL &amp; <i>TOOLS</i></h2>
-      <div>
-          <span>HTML5</span>
-          <span>CSS3</span>
-          <span>SCSS</span>
+    <section className="section_skill bg_change" ref={skillRef => ref.current[2] = skillRef}>
+      <div className="bx_con_1400">
+        <div className="group_circle">
+          <i className="annotation">* 관심분야 / 학습 중</i>
+          <h2 className="title_h2">SKILL &amp; <i>TOOLS</i></h2>
+          <div>
+              <span>HTML5</span>
+              <span>CSS3</span>
+              <span>SCSS</span>
+          </div>
+          <div>
+              <span>Javascript</span>
+              <span>ES6</span>
+              <span>JQuery</span>
+              <span className="_blue">Typescript</span>
+              <span className="_blue">React</span>
+              <span>Flutter</span>
+          </div>
+          <div>
+              <span>Zeplin</span>
+              <span>Figma</span>
+              <span>Photoshop</span>
+          </div>
+          <div>
+              <span>Git</span>
+              <span>Github</span>
+              <span>Soucetree</span>
+              <span>Visual Studio Code</span>
+              <span>Spring Tool Suite 4</span>
+          </div>
+        </div>
       </div>
-      <div>
-          <span>Javascript</span>
-          <span>ES6</span>
-          <span>JQuery</span>
-          <span className="_blue">Typescript</span>
-          <span>React</span>
-          <span>Flutter</span>
-      </div>
-      <div>
-          <span>Zeplin</span>
-          <span>Figma</span>
-          <span>Photoshop</span>
-      </div>
-      <div>
-          <span>Git</span>
-          <span>Github</span>
-          <span>Soucetree</span>
-          <span>Visual Studio Code</span>
-          <span>Spring Tool Suite 4</span>
-      </div>
-    </div>
-    </>
+    </section>
   )
 }
 
-export default Skill;
+export default forwardRef(Skill);
