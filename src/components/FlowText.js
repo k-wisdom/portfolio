bfe2b234569a7c26e7e24064c0cac34536cf0af1
jihye <bnm1354@gmail.com>
2023-10-20 00:04:45 +0900

    About 이미지 변경 및 레이아웃 수정

--- src/components/FlowText.js
+++ src/components/FlowText.js
@@ -2,9 +2,13 @@ import React, { useEffect } from 'react';
 import styled from "styled-components";
 
 function FlowText(props){
+
+  const divName = props.name ? props.name : 'flow_wrapper';
+
   useEffect(() => {    
-    const flowbox = document.querySelector('.flow_wrapper');
-    const scrollFlow = new ScrollFlow(flowbox);
+    
+    const flowbox = document.querySelector('.'+divName);
+    const scrollFlow = new ScrollFlow(flowbox, props.toLeft);
 
     const handleScroll = () => {
       scrollFlow.animate()
@@ -19,9 +23,10 @@ function FlowText(props){
     };
   });
 
+
   return(
     <>
-      <Wrapper className="flow_wrapper">
+      <Wrapper className={divName}>
         <div className="flow_wrap">
           <Text>{props.val}</Text>
           <Text>{props.val}</Text>
@@ -32,50 +37,48 @@ function FlowText(props){
   )
 }
 
-
 class ScrollFlow{
-    constructor(flowbox){
-        this.flowbox = flowbox;
-        this.flowCon = flowbox.querySelector('.flow_wrap');
-    }
+  constructor(flowbox, toLeft){
+    this.flowbox = flowbox;
+    this.flowCon = flowbox.querySelector('.flow_wrap');
+    this.toLeft = toLeft;
+  }
 
-    get inView() {
-        const box = this.flowbox.getBoundingClientRect();
-        return box.top <= window.innerHeight && box.top >= -box.height
-    }
+  get inView() {
+    const box = this.flowbox.getBoundingClientRect();
+    return box.top <= window.innerHeight && box.top >= -box.height
+  }
 
-    get offset(){
+  get offset(){
     return this.flowCon.getBoundingClientRect().top * 0.5;
+  }
+  
+  animate(){
+    if (!this.inView){
+        return;
     }
-    
-    animate(){
-        if (!this.inView){
-            return;
-        }
-        let offsetX = this.offset * 0.5;
-        this.flowCon.style.transform = `translateX(-${offsetX}px)`;
+    let offsetX = this.offset * 0.5;
+
+    if(!this.toLeft){
+      this.flowCon.style.transform = `translateX(-${offsetX}px)`;
+    }else{
+      this.flowCon.style.transform = `translateX(${offsetX}px)`;
     }
+  }
 }
 
 const Wrapper = styled.div`
   position:relative;
   overflow:hidden;
   z-index:9;
-  padding: 2rem 2rem;
   width: 100%;
   box-sizing:border-box;
-  border-top: 1px solid #000;
-  border-bottom: 1px solid #000;
-  background-color:#fff;
-  font-size:2.2rem;
   white-space:nowrap;
 `;
 
 const Text = styled.span`
   margin-right:1rem;
   font-weight:bold;
-  text-shadow: 1px 1px 0 #000000, 1px -1px 0 #000000, -1px 1px 0 #000000, -1px -1px 0 #000000, 1px 0px 0 #000000, 0px 1px 0 #000000, -1px 0px 0 #000000, 0px -1px 0 #000000;
-  color:#fff;
 `;
 
 export default FlowText;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,35 @@
+import React, {forwardRef} from 'react';
+import '../assets/scss/pages/_about.scss'
 
-
-function About(){
+function About(props, ref){
   return(
-    <div className="bx_con_1600">
-      <div className="about_con">
-          <div className="about_left">
-            <b className="top_text"><span>Work</span><br/> with me</b>
-            <p>
-              안녕하세요.<br/>
-              저는 웹 퍼블리싱, 프론트엔드 업무를 진행했으며<br/>
-              특히 퍼블리싱은 3년간 혼자 도맡아 책임감을 갖고 작업에 임했습니다.<br/>
-              효율을 중시하기 때문에 집중력이 좋고, 꼼꼼히 계획을 통해 업무를 수행합니다.
-            </p>
-          </div>
-          <div className="frame">
-            <ul>
-                <li><b>Name</b>김지혜</li>
-                <li><b>Birth</b>97.02.24</li>
-                <li><b>Phone</b>010-3011-0378</li>
-                <li><b>E-mail</b>bnm1354@gmail.com</li>
-                <li><b>Github</b>https://github.com/k-wisdom</li>
-            </ul>  
-            <img src={require("../assets/img/JH_0267.png")} alt="프로필 이미지"/>      
-          </div>
+    <section className="section_about toggle" ref={aboutRef => ref.current[0] = aboutRef}>
+      <div className="bx_con_1400">
+        <div className="about_con">
+            <div className="about_left">
+              <b className="top_text"><span className="point_text">Work</span><br/> with me</b>
+              <p>
+                안녕하세요.<br/>
+                저는 웹 퍼블리싱, 프론트엔드 업무를 진행했으며,<br/>
+                특히 퍼블리싱은 3년간 혼자 도맡아 책임감을 갖고 작업에 임했습니다.<br/>
+                효율을 중시하기 때문에 집중력이 좋고, 꼼꼼히 계획을 통해 업무를 수행합니다.<br/>
+                현실에 안주하지않고 적극적으로 저의 역량을 높히고 개발 분야에 대한 시야를 넓혀가고있습니다.
+              </p>
+            </div>
+            <div className="frame">
+              <ul>
+                  <li><b>Name</b>김지혜</li>
+                  <li><b>Birth</b>97.02.24</li>
+                  <li><b>Phone</b>010-3011-0378</li>
+                  <li><b>E-mail</b>bnm1354@gmail.com</li>
+                  <li><b>Github</b>https://github.com/k-wisdom</li>
+              </ul>  
+              <img src={require("../assets/img/jh.png")} alt="프로필 이미지"/>      
+            </div>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
-export default About;
+export default forwardRef(About);
