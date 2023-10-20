2c6ca53299bb2aecf898c11e8cb8e98eb79cd579
jihye <bnm1354@gmail.com>
2023-10-20 12:49:45 +0900

    Header 메뉴 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-function Header(){
+function Header({menuRef}){
 
   const [menuFlg, setMenuFlg] = useState(false);
+  const [navIndex, setNavIndex] = useState(null);
 
+  const navItems = [
+    { idx: 0, name: 'ABOUT ME' },
+    { idx: 1, name: 'FEATURED PROJECT' },
+    { idx: 2, name: 'MY SKILL' },
+    { idx: 3, name: 'WORKS' },
+    { idx: 4, name: 'CONTACT' },
+  ]
+  
   const menuFn = () => {
 
+    const header = document.querySelector('.fixed_header');
     const menuWrap = document.querySelector('#menuWrap');
     const bgWraps = document.querySelectorAll('.menu_background_wrap div');
     const gnb = document.querySelector('.gnb'); 
 
     if(!menuFlg){
+        header.style.zIndex = 99999;
         menuWrap.classList.add('active');
         bgWraps.forEach((item, index) => {
             item.style.transition = 'transform 1.3s';
@@ -21,6 +32,7 @@ function Header(){
         })
         setTimeout(()=>{ gnb.style.display = 'block';},600)
     }else{
+        setTimeout(()=>{ header.style.zIndex = 98;},600)
         menuWrap.classList.remove('active');
         let bgWrapsReverse = Array.prototype.slice.call(bgWraps).reverse();
         bgWrapsReverse.forEach((item, index) => {
@@ -35,24 +47,37 @@ function Header(){
 
     setMenuFlg(!menuFlg);
   }
+  
+  useEffect(() => {
+    menuRef.current[navIndex]?.scrollIntoView();
+    setNavIndex(null)
+    
+    //메뉴 닫기
+    if(menuFlg === true) menuFn();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [menuRef, navIndex]);
 
   return(
     <header className="fixed_header">
       <div className="bx_con_1600 header_content">
         <span>KIMJIHYE</span>
         <nav className="menu_wrap" id="menuWrap">
-          <button className="btn_menu" onClick={menuFn}><span className="hide">메뉴</span></button>
+          <button className="btn_menu" onClick={() => menuFn()}><span className="hide">메뉴</span><i></i><i></i><i></i></button>
           <div className="menu_background_wrap">
             <div></div>
             <div></div>
             <div></div>
           </div>
           <ul className="gnb" style={{display:"none"}}>
-              <li><a href="#about">ABOUT ME</a></li>
-              <li><a href="#project">MAIN PROJECT</a></li>
-              <li><a href="#skill">MY SKILL</a></li>
-              <li><a href="#carrear">CARREAR</a></li>
-              <li><a href="#contact">CONTACT ME</a></li>
+              {navItems.map(item => {
+                return <li 
+                        key={item.idx} 
+                        onClick={() => {
+                          setNavIndex(item.idx);
+                        }}>
+                          <span tabIndex="0">{item.name}</span>
+                        </li>
+              })}
           </ul>
         </nav>
       </div>
